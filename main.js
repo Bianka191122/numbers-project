ad4c53f9ce1d95d6ccb8e45a55d34cc4af874e8d
Bianka191122 <nagybiankarebeka22@gmail.com>
2024-03-22 13:34:09 +0100

    showlives

--- main.js
+++ main.js
@@ -1,13 +1,18 @@
 import { genRandNumber } from "./genRandNumber.js";
 import { compare }from "./compare.js";
 
-
 let randNr = genRandNumber(1,100);
 console.log(randNr);
+
+let lives = showLives();
+console.log(lives);
+let nr = 5;
+
 /*
 let result = compare(50, randNr);
 console.log(result);
 */
+
 document.querySelector('.checkBtn').addEventListener('click', check)
 
 function check(){
@@ -15,4 +20,5 @@ function check(){
     console.log(userNumber);
     let result = compare(userNumber, randNr)
     document.querySelector('.megoldas').innerHTML = result
+    document.querySelector('.probalkozasok').innerHTML = lives
 }
