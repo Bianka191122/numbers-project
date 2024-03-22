134dcb6e1cd69f6346b280ba507cf2f42ff4224b
govorDori <dorigovor2006@gmail.com>
2024-03-22 14:25:12 +0100

    index valtoztatas

--- compare.js
+++ compare.js
@@ -1,5 +1,8 @@
 export function compare(userNumber, randNumber){
-    if(userNumber<randNumber){
+    if(userNumber<0 || userNumber>100){
+        return 'Helytelen szám!'
+    }
+    else if(userNumber<randNumber){
         return 'A te számod kisebb!'
     }
     else if(userNumber>randNumber){
